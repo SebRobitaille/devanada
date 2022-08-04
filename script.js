7839e10e53d599b5d43d1d64820a13395ea167f9
Sebastian Robitaille <95282730+SebRobitaille@users.noreply.github.com>
2022-08-04 04:23:41 -0700

    FInished control buttons

--- script.js
+++ script.js
@@ -3,31 +3,61 @@ const sectBtns = document.querySelectorAll(".controls");
 const sectBtn = document.querySelectorAll(".control");
 const allSections = document.querySelector(".main-content");
 
-function pageTransitions() {
-  // Button click -> make section active class
+function PageTransitions() {
+  //button click active class
+
   for (let i = 0; i < sectBtn.length; i++) {
     sectBtn[i].addEventListener("click", function () {
       let currentBtn = document.querySelectorAll(".active-btn");
-      currentBtn[0].className = currentBtn.className.replace("active-btn", "");
+      currentBtn[0].className = currentBtn[0].className.replace(
+        "active-btn",
+        ""
+      );
       this.className += " active-btn";
     });
   }
+
+  // Sections active class
+  allSections.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      // remove selected from the other btns
+      sectBtn.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+      // e.target.classList.add("active"); <--- might be useless
+      // hide other sections
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  });
+
+  // // Toggle theme
+  // const themeBtn = document.querySelector(".theme-btn");
+  // themeBtn.addEventListener("click", () => {
+  //   let element = document.body;
+  //   element.classList.toggle("light-mode");
+  // });
 }
 
-pageTransitions();
+PageTransitions();
 
 // Start of the typing animation within the header
-var TxtRotate = function(el, toRotate, period) {
+var TxtRotate = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
   this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
+  this.txt = "";
   this.tick();
   this.isDeleting = false;
 };
 
-TxtRotate.prototype.tick = function() {
+TxtRotate.prototype.tick = function () {
   var i = this.loopNum % this.toRotate.length;
   var fullTxt = this.toRotate[i];
 
@@ -37,38 +67,41 @@ TxtRotate.prototype.tick = function() {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
 
   var that = this;
   var delta = 300 - Math.random() * 100;
 
-  if (this.isDeleting) { delta /= 2; }
+  if (this.isDeleting) {
+    delta /= 2;
+  }
 
   if (!this.isDeleting && this.txt === fullTxt) {
     delta = this.period;
     this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
+  } else if (this.isDeleting && this.txt === "") {
     this.isDeleting = false;
     this.loopNum++;
     delta = 500;
   }
 
-  setTimeout(function() {
+  setTimeout(function () {
     that.tick();
   }, delta);
 };
 
-window.onload = function() {
-  var elements = document.getElementsByClassName('txt-rotate');
-  for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
-    var period = elements[i].getAttribute('data-period');
+window.onload = function () {
+  var elements = document.getElementsByClassName("txt-rotate");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-rotate");
+    var period = elements[i].getAttribute("data-period");
     if (toRotate) {
       new TxtRotate(elements[i], JSON.parse(toRotate), period);
     }
   }
   // INJECT CSS
   var css = document.createElement("style");
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid var(--color-grey0) }";
+  css.innerHTML =
+    ".txt-rotate > .wrap { border-right: 0.08em solid var(--color-grey0) }";
   document.body.appendChild(css);
 };
