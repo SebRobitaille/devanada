a6a161cb8988d551a7b7520f2bb4b45134bb00e8
Sebastian Robitaille <95282730+SebRobitaille@users.noreply.github.com>
2023-04-09 03:34:30 -0700

    done contact

--- dist/script.76d4bba4.js
+++ dist/script.76d4bba4.js
@@ -317,7 +317,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52520" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64651" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
