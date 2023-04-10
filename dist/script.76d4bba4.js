60afd9c0893a65799110631cf76c2140a51ab871
Sebastian Robitaille <95282730+SebRobitaille@users.noreply.github.com>
2023-04-10 04:51:54 -0700

    glow cards on approach page

--- dist/script.76d4bba4.js
+++ dist/script.76d4bba4.js
@@ -351,7 +351,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52852" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64832" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
